e9b73f14c19d43899d7826f38f70360c71d08058
Michael Kraxner <kraxner@ifs.tuwien.ac.at>
2013-07-10 17:45:52 +0200

    adds measures related to audio files

--- DP/doc/generateDoc.sh
+++ DP/doc/generateDoc.sh
@@ -2,7 +2,7 @@
 
 BASE=http://purl.org/DP
 
-DOC_GEN="java -jar parrot-jar-with-dependencies.jar "
+DOC_GEN="java -jar parrot-jar-with-dependencies.jar"
 
 $DOC_GEN -i $BASE/quality -o quality_.html
 $DOC_GEN -i $BASE/quality/categories -o quality_categories.html
@@ -16,4 +16,3 @@ $DOC_GEN -i $BASE/control-policy/modalities -o control-policy_modalities.html
 $DOC_GEN -i $BASE/control-policy/qualifiers -o control-policy_qualifiers.html
 
 $DOC_GEN -i $BASE/preservation-case -o preservation-case_.html
-
